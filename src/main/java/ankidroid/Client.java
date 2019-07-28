e41992b0a8c7db8d524babb4b3a8f1bd329142aa
RahulRj09 <rahul18@navgurukul.org>
2019-07-28 11:56:52 +0530

    add. write the anki cards data in json file

--- src/main/java/ankidroid/Client.java
+++ src/main/java/ankidroid/Client.java
@@ -6,16 +6,15 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         Deck deck = new Deck("Technical");
-        deck.addCard("rahul","joshi");
-        deck.addCard("rahul1","joshi1");
-        for (Card cards: deck.getCards()) {
+        deck.addCard("rahul", "joshi");
+        deck.addCard("rahul1", "joshi1");
+        for (Card cards : deck.getCards()) {
             Scanner input = new Scanner(System.in);
             System.out.println(cards.front);
             System.out.println("Show Answer");
-            String  showAnswer = input.nextLine();
+            String showAnswer = input.nextLine();
             System.out.println(cards.back);
             String difficultlyLevel = input.nextLine();
         }
-
     }
 }
--- src/main/java/ankidroid/Deck.java
+++ src/main/java/ankidroid/Deck.java
@@ -1,7 +1,11 @@
 package ankidroid;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.codehaus.jackson.map.ObjectMapper;
 
 public class Deck {
     public String name;
@@ -14,6 +18,16 @@ public class Deck {
     public void addCard(String front, String back) {
         Card create = new Card(front, back);
         cards.add(create);
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            mapper.writeValue(new File("/Users/rahul.joshi/ankidroidd/src/main/java/ankidroid/cards.json"),
+                    cards);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public List<Card> getCards() {
